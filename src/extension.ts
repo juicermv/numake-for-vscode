83936e3b9c1e1fe8a4fed1bc2050fdf0145154a7
Yoni <yonimeller@protonmail.com>
2024-05-30 23:47:04 +0300

    added build

--- src/extension.ts
+++ src/extension.ts
@@ -5,99 +5,179 @@ import 'child_process';
 import { extensions } from 'vscode';
 import { exec } from 'child_process';
 import { Interface } from 'mocha';
+import { Console } from 'console';
 
 interface NuMakeJSON {
-	workspace: string,
-	working_directory: string,
-	output: string | null,
-	toolset_compiler: string | null,
-	toolset_linker: string | null,
-	targets: { [name: string]: { [type: string]: NuMakeTarget } }
+	workspace: string;
+	working_directory: string;
+	output: string | null;
+	toolset_compiler: string | null;
+	toolset_linker: string | null;
+	targets: { [name: string]: NuMakeTarget };
 }
 
 interface NuMakeTarget {
-	name: string
-	defines: string[],
-	include_paths: string[]
+	name: string;
+	defines: string[];
+	include_paths: string[];
+	vscode_properties: NuMakeTargetVSCodeProperties;
 }
 
+interface NuMakeTargetVSCodeProperties {
+	compiler_path: string;
+	default_includes: string[];
+	intellisense_mode: string;
+}
 
 interface CCPPConfiguration {
-	configurations: CCPPConfigurationItem[]
+	configurations: CCPPConfigurationItem[];
 }
 
 interface CCPPConfigurationItem {
-	name: string,
-	includePath: string[],
-	defines: string[]
+	name: string;
+	includePath: string[];
+	defines: string[];
+	compilerPath: string;
+	intellisenseMode: string;
 }
 
 let nuMakePath: string | undefined;
 let nuMakeScript: string;
 let nuMakeConfig: NuMakeJSON | undefined;
-let nuMakeTargets: string[] = [];
 
-let selectedTarget: string;
+let targets: string[];
+let selectedTarget: string | undefined;
 
-function inspect() {
-	if (nuMakePath === undefined) {
-		vscode.window.showErrorMessage("NuMake executable not provided!");
-		return;
-	}
+let output: vscode.OutputChannel;
 
-	if (vscode.workspace.workspaceFolders === undefined) { return; }
+async function select() {
+	inspect(async (targets) => {
+		if (targets.length > 0) {
+			selectedTarget = await vscode.window.showQuickPick(targets, {
+				canPickMany: false,
+			});
 
-	exec(nuMakePath + ' inspect -w ' + ' "' + vscode.workspace.workspaceFolders[0].uri.fsPath + '"' + ' -f' + ' "' + nuMakeScript + '" -q', async (err, stdout, stderr) => {
-		if (err !== null) {
-			vscode.window.showErrorMessage("Error executing NuMake!");
-			console.error(err);
+			await vscode.commands.executeCommand('C_Cpp.ConfigurationSelect', [
+				selectedTarget,
+			]);
 		} else {
-			//console.log(stdout);
-			nuMakeConfig = JSON.parse(stdout);
-			console.log(nuMakeConfig);
-			let c_cpp_config: CCPPConfiguration = {configurations:[]};
-			for (const targetName in nuMakeConfig?.targets) {
-				for (const targetType in nuMakeConfig?.targets[targetName]){
-					let target = nuMakeConfig?.targets[targetName][targetType];
-
-					let newConfig: CCPPConfigurationItem = {
-						name: target.name,
-						includePath: target.include_paths,
-						defines: target.defines
-					};
-
-					c_cpp_config.configurations.push(newConfig);
-				}
-			}
-
-			if (vscode.workspace.workspaceFolders === undefined) { return; }
-			vscode.workspace.fs.writeFile(
-				vscode.Uri.joinPath(vscode.workspace.workspaceFolders[0].uri, ".vscode", "c_cpp_properties.json"),
-				Buffer.from(JSON.stringify(c_cpp_config))
-			);
+			vscode.window.showWarningMessage('No targets were found!');
 		}
 	});
 }
 
-function selectTarget(name: string) {
-	console.log(name);
-	if (name !in nuMakeTargets) {
-		vscode.window.showErrorMessage("NuMake target '" + name + "' not found!");
+async function build() {
+	while (selectedTarget === undefined || !targets.includes(selectedTarget)) {
+		await select();
+	}
+
+	if (nuMakePath === undefined) {
+		vscode.window.showErrorMessage('nuMake executable path not provided!');
 		return;
 	}
 
-	for (const key in nuMakeConfig?.targets[name]) {
-		let target = nuMakeConfig.targets[name][key];
-		
+	if (vscode.workspace.workspaceFolders === undefined) {
+		return;
 	}
+
+	const command =
+		nuMakePath +
+		' build ' +
+		selectedTarget +
+		' -w "' +
+		vscode.workspace.workspaceFolders[0].uri.fsPath +
+		'"' +
+		' -f' +
+		' "' +
+		nuMakeScript +
+		'"';
+
+	const terminal = vscode.window.createTerminal('nuMake Build: ' + selectedTarget);
+	terminal.sendText(command, true)
+	terminal.show()
+}
+
+function inspect(
+	callback: ((targets: string[]) => void) | undefined = undefined
+) {
+	if (nuMakePath === undefined) {
+		vscode.window.showErrorMessage('nuMake executable path not provided!');
+		return;
+	}
+
+	if (vscode.workspace.workspaceFolders === undefined) {
+		return;
+	}
+
+	targets = [];
+
+	exec(
+		nuMakePath +
+			' inspect -w ' +
+			' "' +
+			vscode.workspace.workspaceFolders[0].uri.fsPath +
+			'"' +
+			' -f' +
+			' "' +
+			nuMakeScript +
+			'" -q',
+		(err, stdout, stderr) => {
+			if (err !== null) {
+				vscode.window.showErrorMessage('Error executing nuMake!');
+				console.error(err);
+			} else {
+				//console.log(stdout);
+				nuMakeConfig = JSON.parse(stdout);
+				console.log(nuMakeConfig);
+				let c_cpp_config: CCPPConfiguration = { configurations: [] };
+				for (const targetName in nuMakeConfig?.targets) {
+					targets.push(targetName);
+					let target = nuMakeConfig.targets[targetName];
+
+					let newConfig: CCPPConfigurationItem = {
+						name: target.name,
+						includePath:
+							target.vscode_properties.default_includes.concat(
+								target.include_paths
+							),
+						defines: target.defines,
+						compilerPath: target.vscode_properties.compiler_path,
+						intellisenseMode:
+							target.vscode_properties.intellisense_mode,
+					};
+
+					c_cpp_config.configurations.push(newConfig);
+				}
+
+				if (vscode.workspace.workspaceFolders === undefined) {
+					return;
+				}
+				vscode.workspace.fs.writeFile(
+					vscode.Uri.joinPath(
+						vscode.workspace.workspaceFolders[0].uri,
+						'.vscode',
+						'c_cpp_properties.json'
+					),
+					Buffer.from(JSON.stringify(c_cpp_config))
+				);
+
+				if (callback !== undefined) {
+					callback(targets);
+				}
+			}
+		}
+	);
 }
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	nuMakePath = vscode.workspace.getConfiguration().get("numake.path");
-	nuMakeScript = vscode.workspace.getConfiguration().get("numake.scriptName", "project.lua");
+	output = vscode.window.createOutputChannel('nuMake');
 
+	nuMakePath = vscode.workspace.getConfiguration().get('numake.path');
+	nuMakeScript = vscode.workspace
+		.getConfiguration()
+		.get('numake.scriptName', 'project.lua');
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -106,21 +186,31 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let numakeRefresh = vscode.commands.registerCommand('numake.refresh', () => {
-		inspect();
-	});
-
-	/*let numakeSelectTarget = vscode.commands.registerCommand("numake.selectTarget", async () => {
-		console.log(nuMakeTargets);
-		const selection = await vscode.window.showQuickPick(nuMakeTargets, { canPickMany: false });
-		if (selection !== undefined) {
-			selectTarget(selection);
+	let numakeRefresh = vscode.commands.registerCommand(
+		'numake.refresh',
+		() => {
+			inspect();
 		}
-	});*/
+	);
+
+	let numakeSelectTarget = vscode.commands.registerCommand(
+		'numake.selectTarget',
+		async () => {
+			await select();
+		}
+	);
+
+	let numakeBuild = vscode.commands.registerCommand(
+		'numake.build',
+		async () => {
+			await build();
+		}
+	);
 
 	context.subscriptions.push(numakeRefresh);
-	//context.subscriptions.push(numakeSelectTarget)
+	context.subscriptions.push(numakeSelectTarget);
+	context.subscriptions.push(numakeBuild);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
